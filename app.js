7213df13076422d1865e88d6ca495c0e555f2303
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-03-04 19:46:42 +0330

    make code modular

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const Binance = require('node-binance-api');
-const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const binance = new Binance().options({
-  APIKEY: 'v5FwYDkW8SPp4a8nw9E33pNYUukelEHGAxtI800rxKOZUfMMVC7ByBn4DRf7tH4Z',
-  APISECRET: 'YTx5HC4fVWOSPHTeNm58lGOoKIUaxCPMAGcE65BtoGV3Owm7otUHgE4v3P2WdNyg',
+  APIKEY: process.env.API_KEY,
+  APISECRET: process.env.API_SECRET,
 });
 
-const db = 'mongodb://localhost:27017/binanceBot';
+const db = process.env.DATABASE_URL;
 mongoose
   .connect(db, {
     useNewUrlParser: true,
@@ -16,47 +18,3 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('Database connect successfuly!'));
-
-const ethSchema = mongoose.Schema(
-  {
-    price: String,
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    orderTime: Date,
-    sellTimeOne: Date,
-    sellTimeTwo: Date,
-    symbol: String,
-  },
-  { versionKey: false }
-);
-
-const Eth = mongoose.model('eth', ethSchema);
-
-const getLastPrice = async () => {
-  try {
-    const resp = await axios.get(
-      'https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT'
-    );
-    await Eth.create({ price: resp.data.price, symbol: resp.data.symbol });
-    let price = await Eth.findOne().sort('-createdAt');
-    let orderTime = price.createdAt.getTime() + 10000; // add 10 sec to buy
-    let sellTimeOne = orderTime + 480000; // add 8 minute to order one
-    let sellTimeTwo = sellTimeOne + 120000; // add 2 minute to order two
-    await Eth.findByIdAndUpdate(price.id, {
-      orderTime,
-      sellTimeOne,
-      sellTimeTwo,
-    });
-    console.log(await Eth.findOne().sort('-createdAt'));
-  } catch (err) {
-    // Handle Error Here
-    console.error(err);
-  }
-};
-
-// setInterval(() => {
-//   getLastPrice();
-// }, 1000);
-getLastPrice();
