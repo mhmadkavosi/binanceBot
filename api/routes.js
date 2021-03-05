b51772499858342c661c39c677479dedbc7d9722
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-03-05 13:33:05 +0330

    oepn orders route

--- api/routes.js
+++ api/routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route('/buy/:symbol').get(controller.createMarketBuy);
 router.route('/sell/:symbol').get(controller.createMarketSell);
 router.route('/balanceOnBTC').get(controller.balanceOnBTC);
+router.route('/openOrders').get(controller.getOpenOrders);
 
 module.exports = router;
