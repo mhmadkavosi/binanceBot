8bc5b70b72a5ef3ac4f6ead97a5c6a115d0eeea2
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-03-05 13:48:57 +0330

    get last price

--- api/controller.js
+++ api/controller.js
@@ -13,27 +13,31 @@ const binance = new Binance().options({
   APISECRET: process.env.API_SECRET,
 });
 
-let symbols = 'ETHUSDT';
+// @desc    get last price
+// @route   /api/lastPrice/:symbols GET
 
-exports.getLastPrice = async () => {
+exports.getLastPrice = async (req, res, next) => {
   try {
+    const { symbols } = req.params;
     const resp = await axios.get(
-      `https://api.binance.com/api/v3/ticker/price?symbol=${symbols}`
+      `https://api.binance.com/api/v3/ticker/price?symbol=${symbols.toUpperCase()}`
     );
-    await model.create({ price: resp.data.price, symbol: resp.data.symbol });
-    let price = await model.findOne().sort('-createdAt');
-    let orderTime = price.createdAt.getTime() + 10000; // add 10 sec to buy
-    let sellTimeOne = orderTime + 480000; // add 8 minute to order one
-    let sellTimeTwo = sellTimeOne + 120000; // add 2 minute to order two
-    await model.findByIdAndUpdate(price.id, {
-      orderTime,
-      sellTimeOne,
-      sellTimeTwo,
+    await balanceModel.create({
+      balance: resp.data.price,
+      symbol: resp.data.symbol,
     });
-    console.log(await model.findOne().sort('-createdAt'));
-  } catch (err) {
-    // Handle Error Here
-    console.error(err);
+    let data = await balanceModel.findOne().sort('-createdAt');
+    res.status(200).json({
+      status: 'Ok',
+      body: {
+        data,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      error,
+    });
   }
 };
 
