36c89fe3855a581e8c8f07af1ccf2cb8e59312f7
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-03-05 11:52:52 +0330

    initialize express server

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-const controller = require('./api/controller');
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./api/routes');
 
 dotenv.config({ path: './config.env' });
+const app = express();
+const port = process.env.PORT;
+const server = app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
+
+app.use(express.json());
+app.use(morgan('dev'));
 
 const db = process.env.DATABASE_URL;
 mongoose
@@ -14,3 +23,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('Database connect successfuly!'));
+
+// Routes
+app.use('/api/', routes);
